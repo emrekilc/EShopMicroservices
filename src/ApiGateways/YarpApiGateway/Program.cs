898ce0d1781148b364d08c0e8554be1b83e9e1e9
emrekilc <98060674+emrekilc@users.noreply.github.com>
2024-08-19 11:21:50 +0300

    sec19

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 
 var app = builder.Build();
 
+app.UseRateLimiter();
+
+app.MapReverseProxy();
 
 app.Run();
